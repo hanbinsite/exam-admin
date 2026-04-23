a49e73d8b3012bfffa1d975fd0411cc035998692
hanbin <1837261009@qq.com>
2026-04-23 23:17:22 +0800

    fix: 清空登录页默认账号密码，移除快捷登录/验证码/注册按钮

--- src/views/_builtin/login/modules/pwd-login.vue
+++ src/views/_builtin/login/modules/pwd-login.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
-import { loginModuleRecord } from '@/constants/app';
 import { useAuthStore } from '@/store/modules/auth';
-import { useRouterPush } from '@/hooks/common/router';
 import { useForm, useFormRules } from '@/hooks/common/form';
-import { $t } from '@/locales';
 
 defineOptions({ name: 'PwdLogin' });
 
 const authStore = useAuthStore();
-const { toggleLoginModule } = useRouterPush();
 const { formRef, validate } = useForm();
 
 interface FormModel {
@@ -18,8 +14,8 @@ interface FormModel {
 }
 
 const model = ref<FormModel>({
-  userName: 'admin@exam.com',
-  password: 'Qkl123456'
+  userName: '',
+  password: ''
 });
 
 const rules = computed<Record<keyof FormModel, App.Global.FormRule[]>>(() => {
@@ -35,87 +31,19 @@ async function handleSubmit() {
   await validate();
   await authStore.login(model.value.userName, model.value.password);
 }
-
-type AccountKey = 'super' | 'admin' | 'teacher';
-
-interface Account {
-  key: AccountKey;
-  label: string;
-  userName: string;
-  password: string;
-}
-
-const accounts = computed<Account[]>(() => [
-  {
-    key: 'super',
-    label: '超级管理员',
-    userName: 'admin@exam.com',
-    password: 'Qkl123456'
-  },
-  {
-    key: 'admin',
-    label: '管理员',
-    userName: 'admin@exam.com',
-    password: 'Qkl123456'
-  },
-  {
-    key: 'teacher',
-    label: '教师',
-    userName: 'admin@exam.com',
-    password: 'Qkl123456'
-  }
-]);
-
-async function handleAccountLogin(account: Account) {
-  await authStore.login(account.userName, account.password);
-}
 </script>
 
 <template>
   <ElForm ref="formRef" :model="model" :rules="rules" size="large" :show-label="false" @keyup.enter="handleSubmit">
     <ElFormItem prop="userName">
-      <ElInput v-model="model.userName" :placeholder="$t('page.login.common.userNamePlaceholder')" />
+      <ElInput v-model="model.userName" placeholder="请输入账号" />
     </ElFormItem>
     <ElFormItem prop="password">
-      <ElInput
-        v-model="model.password"
-        type="password"
-        show-password-on="click"
-        :placeholder="$t('page.login.common.passwordPlaceholder')"
-      />
+      <ElInput v-model="model.password" type="password" show-password-on="click" placeholder="请输入密码" />
     </ElFormItem>
-    <ElSpace direction="vertical" :size="24" class="w-full" fill>
-      <div class="flex-y-center justify-between">
-        <ElCheckbox>{{ $t('page.login.pwdLogin.rememberMe') }}</ElCheckbox>
-        <ElButton text @click="toggleLoginModule('reset-pwd')">
-          {{ $t('page.login.pwdLogin.forgetPassword') }}
-        </ElButton>
-      </div>
-      <ElButton type="primary" size="large" round block :loading="authStore.loginLoading" @click="handleSubmit">
-        {{ $t('common.confirm') }}
-      </ElButton>
-      <div class="flex-y-center justify-between gap-12px">
-        <ElButton class="flex-1" size="default" @click="toggleLoginModule('code-login')">
-          {{ $t(loginModuleRecord['code-login']) }}
-        </ElButton>
-        <ElButton class="flex-1" size="default" @click="toggleLoginModule('register')">
-          {{ $t(loginModuleRecord.register) }}
-        </ElButton>
-      </div>
-      <ElDivider class="text-14px text-#666 !m-0">{{ $t('page.login.pwdLogin.otherAccountLogin') }}</ElDivider>
-      <div class="flex-center gap-12px">
-        <ElButton
-          v-for="item in accounts"
-          :key="item.key"
-          size="default"
-          type="primary"
-          :disabled="authStore.loginLoading"
-          @click="handleAccountLogin(item)"
-        >
-          {{ item.label }}
-        </ElButton>
-      </div>
-    </ElSpace>
+    <ElButton type="primary" size="large" round block :loading="authStore.loginLoading" @click="handleSubmit">
+      登录
+    </ElButton>
   </ElForm>
 </template>
 
